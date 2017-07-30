ac04d0ba92403e7cb3421fdbfb5c29cdba183bef
lesses <767733103@qq.com>
2017-07-30 21:58:45 +0800

    start music

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <vuemusic></vuemusic>
+  </div>
+</template>
+
+<script>
+import vuemusic from './components/music.vue'
+export default {
+  components: {
+    vuemusic,
+  }
+}
+</script>
+
+<style>
+
+</style>
