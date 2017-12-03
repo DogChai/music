e9176e670be9920a4f94fa5d823f69755c646173
softeyes <767733103@qq.com>
2017-12-03 18:53:25 +0800

    修复路径

--- beta1/src/components/music.vue
+++ beta1/src/components/music.vue
@@ -211,7 +211,7 @@
 			getLrcFile(n) {
 				// ../../music/static/lrc/  打包请修改
 				if(this.$refs.listul2.getAttribute('myplay') == 'false') {
-					axios.get('../../music/static/lrc/'+ n + '.lrc').then((response) => {
+					axios.get('http://softeyes.cc/music/beta1/static/lrc/'+ n + '.lrc').then((response) => {
 						if(response.data == '纯音乐,请欣赏' || response.data == '后摇,请欣赏') {
 							this.lrcData = response.data;
 							this.showLrc();
